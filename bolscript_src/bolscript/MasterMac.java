b89e26a9d5dbeb519558db509e53675d29b8f24f
Hannes Oud <hannes.oud.dev@gmail.com>
2019-04-15 23:54:35 +0200

    Merge MasterMac and MasterMacSnowleopard

--- bolscript_src/bolscript/MasterMac.java
+++ bolscript_src/bolscript/MasterMac.java
@@ -7,6 +7,14 @@ import basics.Debug;
 import bolscript.config.GuiConfig;
 
 import com.apple.eawt.Application;
+import com.apple.eawt.AppEventListener;
+import com.apple.eawt.Application;
+import com.apple.eawt.PreferencesHandler;
+import com.apple.eawt.QuitHandler;
+import com.apple.eawt.QuitResponse;
+import com.apple.eawt.QuitStrategy;
+import com.apple.eawt.AppEvent.PreferencesEvent;
+import com.apple.eawt.AppEvent.QuitEvent;
 import com.apple.eawt.ApplicationEvent;
 import com.apple.eawt.ApplicationListener;
 
@@ -45,6 +53,10 @@ public class MasterMac extends Master implements ApplicationListener {
 	public void init() {
 		super.init();
 		application = Application.getApplication();
+		application = Application.getApplication();
+		application.setPreferencesHandler(this);
+		application.setQuitHandler(this);
+		application.setQuitStrategy(QuitStrategy.SYSTEM_EXIT_0);
 		application.addPreferencesMenuItem();
 		application.setEnabledPreferencesMenu(true);
 		application.addApplicationListener(this);
@@ -62,30 +74,36 @@ public class MasterMac extends Master implements ApplicationListener {
 		super.showPreferences();
 		GuiConfig.setVisibleAndAdaptFrameLocation(browserFrame);
 	}
-	
-	public void handleAbout(ApplicationEvent arg0) {
-	}
 
-	public void handleOpenApplication(ApplicationEvent arg0) {
-	}
+	@Override
+	public void revealFileInOSFileManager(String filename) {
+		String script =
+				"tell application \"Finder\" \n" +
+						"reveal (POSIX file \"/" + filename + "\") \n" +
+						"activate \n"+
+						"end tell";
 
-	public void handleOpenFile(ApplicationEvent arg0) {
-	}
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("AppleScript");
 
-	public void handlePreferences(ApplicationEvent arg0) {
-		showPreferences();
-	}
-
-	public void handlePrintFile(ApplicationEvent arg0) {
-	}
-
-	public void handleQuit(ApplicationEvent arg0) {
-		if (this.prepareExit()) {
-			System.exit(0);
+		try {
+			engine.eval(script);
+		} catch (ScriptException e) {
+			Debug.critical(this, "AppleScript for revealing file could not be run");
+			e.printStackTrace();
 		}
 	}
 
-	public void handleReOpenApplication(ApplicationEvent arg0) {
-	}
 
+	@Override
+	public void handleQuitRequestWith(QuitEvent arg0, QuitResponse arg1) {
+		boolean safelyQuit = this.prepareExit();
+		if (safelyQuit) {
+			arg1.performQuit();
+		} else {
+			arg1.cancelQuit();
+		}
+	}
 }
+
+
